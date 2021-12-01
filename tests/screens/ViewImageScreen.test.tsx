941d3073f395416d4f52e74a25a18e5a4e4198d6
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-12-01 20:39:28 -0300

    test: first test for ViewImageScreen

---
+++ tests/screens/ViewImageScreen.test.tsx
@@ -0,0 +1,8 @@
+import { shallow } from "enzyme"
+import { ViewImageScreen } from "../../app/screens/ViewImageScreen"
+
+describe('ViewImageScreen', () => {
+  it('snapshot matches', () => {
+    expect(shallow(<ViewImageScreen />)).toMatchSnapshot();
+  });
+});
