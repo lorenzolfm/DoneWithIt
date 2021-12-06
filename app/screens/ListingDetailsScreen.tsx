ff182f6b2077dc677a25c872e8f21f2830196bd2
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-12-06 15:17:53 -0300

    feat: first commit for ListingDetailsScreen

---
+++ app/screens/ListingDetailsScreen.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Image, View, StyleSheet } from "react-native";
+
+import { ListItem } from '../components/ListItem';
+import { AppText } from "../components/AppText";
+
+import colors from '../config/colors';
+
+export const ListingDetailsScreen = () => {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/mosh.jpg')}
+            title="Mosh Hamedani"
+            subtitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 40,
+  }
+});
