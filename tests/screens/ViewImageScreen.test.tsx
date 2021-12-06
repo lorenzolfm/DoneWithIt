fdf038e761d1103eb65dd45e1e2837f892d30a6d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-12-06 18:57:34 -0300

    DONE with styling section

--- tests/screens/ViewImageScreen.test.tsx
+++ tests/screens/ViewImageScreen.test.tsx
@@ -1,5 +1,12 @@
-import { shallow } from "enzyme"
-import { ViewImageScreen } from "../../app/screens/ViewImageScreen"
+import { shallow } from "enzyme";
+import { ViewImageScreen } from "../../app/screens/ViewImageScreen";
+
+jest.mock('@expo/vector-icons', () => {
+  const { View } = require('react-native');
+  return {
+    MaterialCommunityIcons: View,
+  }
+});
 
 describe('ViewImageScreen', () => {
   it('snapshot matches', () => {
