d2e578cb54136c066d29a3b337125fa9c93865e1
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-12-08 17:30:51 -0300

    feat: first commit for AccountScreen

---
+++ app/screens/AccountScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ListItem } from '../components/ListItem';
+
+import { Screen } from '../components/Screen';
+
+export const AccountScreen = () => {
+  return (
+    <Screen>
+      <ListItem
+        title="Mosh Hamedani"
+        subtitle="programmingwithmosh@gmail.com"
+      />
+    </Screen>
+  );
+};
