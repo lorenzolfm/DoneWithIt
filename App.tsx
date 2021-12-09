14aa8e132a2ec53854787c9d3f489f569fda87cd
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-12-09 10:17:36 -0300

    DONE: Lists

--- App.tsx
+++ App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import { Screen } from './app/components/Screen';
-import { Icon } from './app/components/Icon';
+import { ListingsScreen } from './app/screens/ListingsScreen';
 
 export default function App() {
   return (
-    <Screen>
-      <Icon
-        name="email"
-        size={50}
-        backgroundColor="red"
-        iconColor="white"
-      />
-    </Screen>
+    <ListingsScreen />
   );
 };
